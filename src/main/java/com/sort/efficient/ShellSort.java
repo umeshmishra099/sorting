0af97f1f19abc21718643b25ce2516f5f8f905d3
umesh <umesmishra099@gmail.com>
2020-06-19 16:58:50 +0530

    removing commented file

--- src/main/java/com/sort/efficient/ShellSort.java
+++ src/main/java/com/sort/efficient/ShellSort.java
@@ -25,53 +25,5 @@ public class ShellSort
         }
       }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*int i, j, k, tmp;
-    int num = input.length;
-    for (i = num / 2; i > 0; i = i / 2)
-    {
-      for (j = i; j < num; j++)
-      {
-        for (k = j - i; k >= 0; k = k - i)
-        {
-          if (input[k + i] >= input[k])
-            break;
-          else
-          {
-            tmp = input[k];
-            input[k] = input[k + i];
-            input[k + i] = tmp;
-          }
-        }
-      }
-    }*/
   }
 }
